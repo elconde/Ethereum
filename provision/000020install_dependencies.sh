3b103911958dc007d81321d109203d486a9a71f9
Robert Conde <robert.conde@fisglobal.com>
2017-06-12 14:22:38 -0400

    more dependencies

--- provision/000020install_dependencies.sh
+++ provision/000020install_dependencies.sh
@@ -5,4 +5,4 @@ set -e  # Exit on error
 
 
 apt-get -y install build-essential automake pkg-config libtool libffi-dev \
-    libgmp-dev python-pip git
+    libgmp-dev python-pip git python-dev libssl-dev
---
+++ provision/000020install_py_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Install any Python dependencies
+set -e  # Exit on error
+
+
+pip install cffi
